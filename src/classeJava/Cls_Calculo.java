6ff52ceb7147f30c5e2485515d9dbdac7e31ed13
Geoguitar <geovanedsilveira@gmail.com>
2017-03-30 23:47:47 -0300

    adicionando arquivos

---
+++ src/classeJava/Cls_Calculo.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classeJava;
+
+/**
+ *
+ * @author geoguitar
+ */
+public class Cls_Calculo {
+    
+    private int valorPercentualDesconto;
+    private float valorDesconto;
+    private float valorFinalAutomovel;
+    
+    public float calculo(String MarcaAutomovel, String AnoFabricacao, float valorBase){
+    
+        switch(MarcaAutomovel){
+        
+            case "FIAT": {
+            
+                if(AnoFabricacao == "2011")
+                    valorPercentualDesconto = 6;
+                else if (AnoFabricacao == "2012")
+                    valorPercentualDesconto = 5;
+            }
+            break;
+            
+             case "VW": {
+            
+                if(AnoFabricacao == "2011")
+                    valorPercentualDesconto = 4;
+                else if (AnoFabricacao == "2012")
+                    valorPercentualDesconto = 3;
+            }
+            break;
+            
+              case "FORD": {
+            
+                if(AnoFabricacao == "2011")
+                    valorPercentualDesconto = 2;
+                else if (AnoFabricacao == "2012")
+                    valorPercentualDesconto = 1;
+            }
+            break;
+        }
+        
+        valorDesconto = valorFinalAutomovel * valorPercentualDesconto/100;
+        valorFinalAutomovel = valorFinalAutomovel - valorDesconto;
+        return valorFinalAutomovel;
+    }
+}
